9873cb32194e2d019d0228f4f4a16df48d56959e
frozenmafia <shivajay295@gmail.com>
2024-01-01 16:02:00 +0530

    login improved

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -11,11 +11,13 @@ router = APIRouter(
 
 @router.post('/login', response_model=schemas.LoginResponse)
 def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
-    user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
+    user:models.User = db.query(models.User).filter(models.User.email == user_credentials.username).first()
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User not found")
     if not utils.verify(user_credentials.password, user.password):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=f'Invalid credentials')
+    if user.online:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f'User already Logged In')
     token_data = {"user_id": user.id}
     access_token = oauth2.create_access_token(token_data)
     db.query(models.User).filter(models.User.email == user_credentials.username).update({"online": True})
